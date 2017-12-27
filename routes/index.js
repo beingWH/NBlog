884c17fc2bcb8c071870b4ee6bd4f4167412d46f
beingLC <29479336@qq.com>
2017-12-27 21:39:37 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
 
-module.exports = router;
+module.exports=function (app) {
+    app.get('/',function (req,res) {
+        res.redirect('/posts')
+    });
+    app.use('/posts',require('./posts'));
+    app.use('/signup',require('./signup'));
+    app.use('/signin',require('./signin'));
+    app.use('/signout',require('./signout'));
+    app.use('/comments',require('./comments'));
+
+}
+
+
+//module.exports = router;
