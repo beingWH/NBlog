b144e69af5de0f95fd909fe8f5e1cd740ac587ef
beingLC <29479336@qq.com>
2018-01-09 21:00:31 +0800

    update

--- models/users.js
+++ models/users.js
@@ -2,7 +2,12 @@ var User=require('../lib/mongo').User
 
 UserModel={
     create:function create(user) {
-        return User.create(user).exec()
+        // return User.create(user).exec()
+        return User.insertOne(user).exec()
+
+    },
+    getUserByName:function getUserByName(name) {
+        return User.findOne({name:name}).addCreatedAt().exec()
     }
 }
 module.exports=UserModel
--- routes/signout.js
+++ routes/signout.js
@@ -4,6 +4,8 @@ var router = express.Router()
 var checkLogin = require('../middlewares/check').checkLogin
 
 router.get('/', checkLogin, function (req, res, next) {
-    res.send('登出')
+    req.session.user=null;
+    req.flash('success','登出成功');
+    res.redirect('/posts');
 })
 module.exports = router
